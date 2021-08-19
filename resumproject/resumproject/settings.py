fddcbfc1185e0a1a70599133dfba4d8c7b3eb88e
Anyijukire <anyijukirjanett@gmail.com>
2021-08-19 10:44:10 +0300

    resume

--- resumproject/resumproject/settings.py
+++ resumproject/resumproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,9 +124,11 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/imgs/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'imgs')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
